ab81281449111423738cc3c8ed6c3d1b702e084f
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-10 13:53:19 +0530

    CBC

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -10,12 +10,12 @@ export class ParentComponent implements OnInit {
   private data:Array<any>;
 
   constructor() { 
-    this.data [
-      {p_id: 111, p_name:'p_one', p_cost:10000},
-      {p_id: 222, p_name:'p_two',p_cost:20000},
-      {p_id: 333, p_name:'p_three',p_cost:30000},
-      {p_id: 444, p_name:'p_four',p_cost:40000},
-      {p_id: 555, p_name:'p_five',p_cost:50000}
+    this.data=[
+      {p_id:111,p_name:'p_one', p_cost:10000},
+      {p_id:222,p_name:'p_two',p_cost:20000},
+      {p_id:333,p_name:'p_three',p_cost:30000},
+      {p_id:444,p_name:'p_four',p_cost:40000},
+      {p_id:555,p_name:'p_five',p_cost:50000}
     ];
   }
 
